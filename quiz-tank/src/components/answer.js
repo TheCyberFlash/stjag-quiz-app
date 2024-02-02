051064ad60b08847392b378697bbad70862b332a
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-02 21:33:02 +0100

    Update answer.js

--- quiz-tank/src/components/answer.js
+++ quiz-tank/src/components/answer.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Answer = ({ answer, onSelect, isCorrect }) => {
-    return (
-        <div className={`answer ${isCorrect ? 'correct' : 'incorrect'}`} onClick={() => onSelect(isCorrect)}>
-        <p>{answer}</p>
-      </div>
-    );
+  return (
+    <div
+      className={`answer ${isCorrect ? "correct" : "incorrect"}`}
+      onClick={() => onSelect(isCorrect)}
+    >
+      <p>{answer}</p>
+    </div>
+  );
 };
 
 export default Answer;
