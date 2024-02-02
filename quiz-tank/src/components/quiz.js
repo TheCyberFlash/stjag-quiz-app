01a888d2d732430acc167d1b03bbd398c8894694
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-02 21:32:51 +0100

    Update quiz.js

--- quiz-tank/src/components/quiz.js
+++ quiz-tank/src/components/quiz.js
@@ -1,43 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import Question from './question';
-import Answer from './answer';
-import data from '../data/questions.json';
+import React, { useState, useEffect } from "react";
+import Question from "./question";
+import Answer from "./answer";
+import data from "../data/questions.json";
 
 const Quiz = () => {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
 
-    const handleAnswer = (isCorrect) => {
-        if (isCorrect) {
-            setScore(score + 1);
-        }
-        nextQuestion();
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
     }
+    nextQuestion();
+  };
 
-    const nextQuestion = () => {
-        if (currentQuestion < data.length - 1) {
-            setCurrentQuestion(currentQuestion + 1);
-        } else {
-            alert('Quiz finished! Your score is: ' + score);
-        }
+  const nextQuestion = () => {
+    if (currentQuestion < data.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      alert("Quiz finished! Your score is: " + score);
     }
+  };
 
-    useEffect(() => {
-        setCurrentQuestion(0);
-        setScore(0);
-    }, []);
+  useEffect(() => {
+    setCurrentQuestion(0);
+    setScore(0);
+  }, []);
 
-    return (
-<div className="quiz">
+  return (
+    <div className="quiz">
       <Question text={data[currentQuestion].question} />
       <div className="answers">
         {data[currentQuestion].options.map((option, index) => (
-          <Answer key={index} text={option.text} isCorrect={option.isCorrect} onSelect={handleAnswer} />
+          <Answer
+            key={index}
+            text={option.text}
+            isCorrect={option.isCorrect}
+            onSelect={handleAnswer}
+          />
         ))}
       </div>
     </div>
-    );
-
+  );
 };
 
 export default Quiz;
